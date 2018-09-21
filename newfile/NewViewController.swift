a28d301d6dd98422d400f1392da84ff676258ec5
Erina Taguchi <kd1257044@st.kobedenshi.ac.jp>
2018-09-21 10:48:18 +0900

    first

---
+++ newfile/NewViewController.swift
@@ -0,0 +1,103 @@
+//
+//  NewViewController.swift
+//  newfile
+//
+//  Created by 田口絵理奈 on 2018/09/11.
+//  Copyright © 2018年 田口絵理奈. All rights reserved.
+//
+
+import UIKit
+
+//ピッカービューの宣言追加
+class NewViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    //ラベルのプロパティ宣言
+    @IBOutlet weak var tenpomei: UILabel!
+    //ピッカービューのプロパティ宣言
+    @IBOutlet weak var myPickerView: UIPickerView!
+    //店舗の一覧表のプロパティ宣言
+    @IBOutlet weak var itiranhyou: UILabel!
+    //コンポートネントに表示する項目名
+    let compos = ["神戸","西宮・尼崎・宝塚","明石・加古川・高砂","姫路・たつの","三田・丹波","豊岡・朝来"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        numberOfRowsInComponent component: Int
+        ) -> Int {
+        return compos.count
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        titleForRow row: Int,
+        forComponent component: Int
+        ) -> String? {
+            return compos[row]
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        didSelectRow row: Int,
+        inComponent component: Int
+    ){
+        
+        
+        tenpomei.text = compos[row]
+        if compos[row] == compos[0] {
+            print("hyougo")
+            itiranhyou.text = "神戸市の店舗です\n" +
+            "aaaa\n" +
+            "bbb\n"
+        } else if compos[row] == compos[1] {
+            print("osaka")
+            itiranhyou.text = "西宮市の店舗です"
+        } else if compos[row] == compos[2] {
+            print("kyoto")
+            itiranhyou.text = "明石市の店舗です"
+        } else if compos[row] == compos[3] {
+            print("siga")
+            itiranhyou.text = "姫路市の店舗です"
+        } else if compos[row] == compos[4] {
+            print("nara")
+            itiranhyou.text = "三田市の店舗です"
+        } else if compos[row] == compos[5]{
+            print("豊岡")
+            itiranhyou.text = "豊岡市の店舗です"
+        } else {
+            print("ks")
+            itiranhyou.text = "エラーです。"
+        }
+    }
+    
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //myPickerViewのデリゲートになる
+        myPickerView.delegate = self
+        //myPickerViewのデータソースになる
+        myPickerView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- newfile/ViewController.swift
+++ newfile/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cTabBar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //スイカの子役カウンター
+    @IBOutlet weak var sgoukei: UILabel!
 
+    @IBAction func spuramai(_ sender: UIStepper) {
+        let num = Int(sender.value)
+        sgoukei.text = String(num)
+    }
+
+    //チェリーの子役カウンター
+    @IBOutlet var tgoukei: UILabel!
+
+    @IBAction func tpuramai(_ sender: UIStepper) {
+        let num1 = Int(sender.value)
+        tgoukei.text = String(num1)
+    }
+    
+    //ベルの子役カウンター
+    @IBOutlet var bgoukei: UILabel!
+    @IBAction func bpuramai(_ sender: UIStepper) {
+        let num2 = Int(sender.value)
+        bgoukei.text = String(num2)
+    }
+    
+    //チャンス目Aの子役カウンター
+    @IBOutlet var tAgoukei: UILabel!
+    @IBAction func tApuramai(_ sender: UIStepper) {
+        let num3 = Int(sender.value)
+        tAgoukei.text = String(num3)
+    }
+    
+    //チャンス目Bの子役カウンター
+    @IBOutlet var tBgoukei: UILabel!
+    @IBAction func tBpuramai(_ sender: UIStepper) {
+        let num4 = Int(sender.value)
+        tBgoukei.text = String(num4)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
